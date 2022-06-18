69192c199a3f43e42340d6d8d6d2e4f9ff138b15
OscarGutierrez312 <49107838+OscarGutierrez312@users.noreply.github.com>
2022-06-18 17:18:16 -0500

    Components_Catl

---
+++ rent-app/pages/Products/preview_product.js
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+
+export default function Preview(){
+
+    return (
+    <footer className="text-white bg-gray-200" >
+      <div className="grid place-items-center bg-gray-200 p-6">    
+        
+        <a className="flex items-center text-gray-600 font-semibold"
+          href="https://www.udistrital.edu.co/inicio"
+          target="_blank"
+          rel="noopener noreferrer"
+        > 
+          
+          <span>© 2022 Copyright:</span>
+          <span>
+            <Image src='/images/Escudo_UD.png' alt="Escudo Distrital" width={50} height={50} />
+          </span>
+        </a>
+           
+      </div>
+        
+      </footer>
+    );
+}
