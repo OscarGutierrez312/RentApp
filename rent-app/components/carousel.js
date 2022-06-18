49885129e9a0cc1263673c6d2a67450eb23011ad
OscarGutierrez312 <49107838+OscarGutierrez312@users.noreply.github.com>
2022-06-18 18:49:09 -0500

    fix_prod

--- rent-app/components/carousel.js
+++ rent-app/components/carousel.js
@@ -21,9 +21,9 @@ export default class NextJsCarousel extends Component {
 		return (
             
 			<div className="flex flex-col bg-gray-200">
-                <div class="text-center bg-gray-50 text-gray-800 p-20 px-6">
-                    <h1 class="text-5xl font-bold mt-0 mb-6">Bienvenido a EasyCar</h1>
-                    <h3 class="text-2xl font-thin mb-8">
+                <div className="text-center bg-gray-50 text-gray-800 p-20 px-6">
+                    <h1 className="text-5xl font-bold mt-0 mb-6">Bienvenido a EasyCar</h1>
+                    <h3 className="text-2xl font-thin mb-8">
                         Pide o Arrienda cualquiera de 
                     los tipos de vehiculos disponibles</h3>
                 </div>
@@ -32,8 +32,8 @@ export default class NextJsCarousel extends Component {
                     return (                        
                     <div key={idx} className="flex-place-items-center z-0">
                         <Image src={i.name} alt={idx} width={'100%'} height={'36'} layout={'responsive'} />
-                        <div class="carousel-caption hidden md:block absolute text-center pb-20 ">
-                            <h5 class="text-xl">First slide label</h5>
+                        <div className="carousel-caption hidden md:block absolute text-center pb-20 ">
+                            <h5 className="text-xl">First slide label</h5>
                             <p>Some representative placeholder content for the first slide.</p>
                         </div>
                         <Link href="/Sesion/logout">
