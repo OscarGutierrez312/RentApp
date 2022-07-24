c58b7d8e1ac32fe250c666ec08300602c7f87b90
saenzjf <saenzjf@gmail.com>
2022-07-24 18:21:47 -0500

    mn1

---
+++ rent-app/pages/info/manual.js
@@ -0,0 +1,11 @@
+import Head from "next/head"
+import Link from "next/link"
+import LayoutInfo from "../components/layout_info"
+
+export default function Manual(providers) {
+    return(
+        <LayoutInfo>
+            
+        </LayoutInfo>
+    )
+}
