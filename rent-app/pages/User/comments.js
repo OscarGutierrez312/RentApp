d84b3c1eeae19af96ad281b8893b42d8e988250a
OscarGutierrez312 <49107838+OscarGutierrez312@users.noreply.github.com>
2022-08-16 15:24:59 -0500

    comments

--- rent-app/pages/User/comments.js
+++ rent-app/pages/User/comments.js
@@ -1,22 +1,30 @@
-
+import Layout from "../../components/layout";
 export default function Comments(){
     return(
-        <>
-            <div className="flex justify-center">
-                <div className="mb-3 xl:w-96">
-                    <label htmlFor="exampleFormControlTextarea1" className="form-label inline-block mb-2 text-gray-700"
-                    >Descripción del Vehículo</label>
+        <Layout>
+            <div className="flex justify-center my-48">
+                <div className="mb-3 xl:w-[70%] xl:h-96">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label text-3xl inline-block mb-2 text-gray-700"
+                    >Escribemos tus comentarios</label>
                     <textarea
-                    className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding
+                    className="form-control block w-full h-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding
                         border border-solid border-gray-300 rounded transition ease-in-out m-0
                         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                     id="exampleFormControlTextarea1"
                     rows="3"
-                    placeholder="Descripción"
+                    placeholder="Observaciones"
                     ></textarea>
+                    <button
+                        className="inline-block px-6 py-2.5 bg-blue-300 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3"
+                        type="submit"
+                        data-mdb-ripple="true"
+                        data-mdb-ripple-color="light"
+                        >
+                        enviar
+                    </button>
                 </div>
             </div>
-        </>
+        </Layout>
     );
 }
 
